f9c954d3d295fd9b258d2493553b4ee4dc1c8b0c
Connor Wilson <connormwilson@gmail.com>
2019-01-24 13:08:07 -0700

    initial commit

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Connor W',
+    profilePic: 'https://scontent.fmkc1-1.fna.fbcdn.net/v/t1.0-1/p160x160/31387771_466579017132234_5222447800006390844_n.jpg?_nc_cat=107&_nc_ht=scontent.fmkc1-1.fna&oh=7ce8a9c49d489fe024e025c2070556aa&oe=5CB30781',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
